1f259790716160ad96345fe176fc8acd95f54038
dinesh0758 <markdinesh94@gmail.com>
2022-02-17 12:10:27 +0530

    demo projects

---
+++ src/org/com/SchoolDetails.java
@@ -0,0 +1,31 @@
+package org.com;
+
+public class SchoolDetails{
+	
+	public void studName() {
+	System.out.println("DINESH MUTHUSAMY");
+	
+	}	
+	
+	public void studAddress() {
+		System.out.println("chennai");
+		
+		}	
+		
+	
+	public void studId() {
+		System.out.println("8526880758");
+		
+		}	
+	public static void main(String[] args) {
+	SchoolDetails stud =new SchoolDetails();	
+     stud.studName();
+	stud.studAddress();
+	stud.studId();
+	
+	
+	
+	
+	}
+
+}
